f3e544aa421af9af3f426282b5cf150adc23ffb2
Joe Mancell <joe.mancell@gmail.com>
2018-04-12 13:51:54 +0000

    kind double things

---
+++ src/Types_mod.f90
@@ -0,0 +1,16 @@
+module Types_mod
+
+use, intrinsic :: iso_fortran_env
+
+implicit none
+! everything is private unless otherwise stated
+private
+
+public :: SP
+
+integer, parameter :: SP = REAL32
+
+contains
+
+  
+end module Types_mod
